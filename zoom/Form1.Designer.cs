d49e2f18dcfaec3ca7cf8d85e0e1762077e9c472
yashwanth <yashwanth9347@gmail.com>
2021-09-15 16:40:54 +0530

    committed

---
+++ zoom/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace zoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(58, 120);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(538, 260);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::zoom.Properties.Resources.moniter;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(510, 248);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(168, 37);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(318, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 392);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ zoom/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace zoom
+{
+    public partial class Form1 : Form
+    {
+        Image imgOriginal;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image zoomimage(Image img, Size s)
+        {
+            Bitmap bm = new Bitmap(img, Convert.ToInt32(img.Width * s.Width), Convert.ToInt32(img.Height * s.Height));
+            Graphics g = Graphics.FromImage(bm);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bm;
+
+        }
+
+        PictureBox pp;
+
+        public static string ConvertFromToTime( string timeHour, string inputFormat, string outputFormat)
+        {
+            var timeFromInput = DateTime.ParseExact(timeHour, inputFormat, null, DateTimeStyles.None);
+            string timeOutput = timeFromInput.ToString(
+                outputFormat,
+                CultureInfo.InvariantCulture);
+            return timeOutput;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+
+
+
+            //string time24 = "20:15:23";
+            //DateTime dt = DateTime.ParseExact(time24, "HH:mm:ss", null, DateTimeStyles.None); ;
+            //String time12 = dt.ToString("0h:mm:ss");
+
+
+            //trackBar1.Minimum = 1;
+            //trackBar1.Maximum = 6;
+            //trackBar1.SmallChange = 1;
+            //trackBar1.LargeChange = 1;
+            //trackBar1.UseWaitCursor = false;
+            //this.DoubleBuffered = true;
+            //pp = new PictureBox();
+
+            //pp.Image = pictureBox1.Image;
+
+            imgOriginal = pictureBox1.Image;
+
+
+            //trackBar1.Minimum = 200;
+            //trackBar1.Maximum = 600;
+            //pictureBox1.Left = (ClientSize.Width - pictureBox1.Width) / 2;
+            //pictureBox1.Top = (ClientSize.Height - pictureBox1.Height) / 2;
+        }
+
+        Image Zoom(Image img, Size size)
+        {
+            Bitmap bmp = new Bitmap(img, img.Width + (img.Width * size.Width / 100), img.Height + (img.Height * size.Height / 100));
+            Graphics g = Graphics.FromImage(bmp);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bmp;
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value > 0)
+            {
+                pictureBox1.Image = Zoom(imgOriginal, new Size(trackBar1.Value, trackBar1.Value));
+            }
+
+            //pictureBox1.Size = new Size(trackBar1.Value, pictureBox1.Size.Height);
+            //pictureBox1.Left = (ClientSize.Width - pictureBox1.Width) / 2;
+            //pictureBox1.Top = (ClientSize.Height - pictureBox1.Height) / 2;
+            //if (trackBar1.Value != 0)
+            //{
+            //    pictureBox1.Image = null;
+            //    pictureBox1.Image=zoomimage(pp.Image,new Size(trackBar1.Value,trackBar1.Value));
+
+            //}
+        }
+    }
+}
